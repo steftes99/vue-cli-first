f1849416bca18aad59f631351b5032ca3bd383b5
steftes99 <stefanotescione83@gmail.com>
2021-07-05 15:59:07 +0200

    inserimento titolo

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>La mia prima app con Vue CLI</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
